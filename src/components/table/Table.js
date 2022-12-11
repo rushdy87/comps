1d47129346bbd7647ee722b8401f40fdb0a4e6b2
“rushdy87” <48268026+m7md7sam@users.noreply.github.com>
2022-12-12 02:29:50 +0300

    Create Table: Make a Feature-Full Data Table

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,14 +1,22 @@
 import React from 'react';
 
-const Table = ({ data }) => {
-  const renderedRows = data.map((hero) => {
-    return (
-      <tr className="border-b" key={hero.name}>
-        <td className="p-3">{hero.name}</td>
-        <td className="p-3">
-          <div className={`p-3 m-2 ${hero.color}`} />
+const Table = ({ data, config, keyFn }) => {
+  const renderedHeaders = config.map((column) => (
+    <th key={column.label}>{column.label}</th>
+  ));
+
+  const renderedRows = data.map((rowData) => {
+    const renderedCells = config.map((column) => {
+      return (
+        <td className="p-3" key={column.label}>
+          {column.render(rowData)}
         </td>
-        <td className="p-3">{hero.score}</td>
+      );
+    });
+
+    return (
+      <tr className="border-b" key={keyFn(rowData)}>
+        {renderedCells}
       </tr>
     );
   });
@@ -16,11 +24,7 @@ const Table = ({ data }) => {
   return (
     <table className="table-auto border-spacing-2">
       <thead>
-        <tr className="border-b-2">
-          <th>Name</th>
-          <th>Color</th>
-          <th>Score</th>
-        </tr>
+        <tr className="border-b-2">{renderedHeaders}</tr>
       </thead>
       <tbody>{renderedRows}</tbody>
     </table>
--- src/pages/table-page/TablePage.js
+++ src/pages/table-page/TablePage.js
@@ -1,19 +1,31 @@
 import Table from '../../components/table/Table';
 
 const TablePage = () => {
+  const keyFn = (hero) => hero.name;
+
   return (
     <div>
-      <Table data={HEROES_Data} />
+      <Table data={HEROES_DATA} config={CONFIG} keyFn={keyFn} />
     </div>
   );
 };
 
 export default TablePage;
 
-const HEROES_Data = [
+const HEROES_DATA = [
   { name: 'Messi', color: 'bg-blue-700', score: 10 },
-  { name: 'Soperman', color: 'bg-red-700', score: 4 },
-  { name: 'Spaiderman', color: 'bg-gray-700', score: 5 },
+  { name: 'Superman', color: 'bg-red-700', score: 4 },
+  { name: 'Wonder Woman', color: 'bg-gray-700', score: 5 },
   { name: 'Joker', color: 'bg-yellow-700', score: 9 },
   { name: 'Thor', color: 'bg-amber-600', score: 6 },
+];
+
+const CONFIG = [
+  { label: 'Name', render: (hero) => hero.name },
+  {
+    label: 'Color',
+    render: (hero) => <div className={`p-3 m-2 ${hero.color}`} />,
+  },
+  { label: 'Score', render: (hero) => hero.score },
+  { label: 'Sample', render: (hero) => hero.name[0] },
 ];
